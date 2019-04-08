240a2499285d3a76dc0abf39a946d711afc9755f
Chitra Limbu <limbu_nikkil@hotmail.co.uk>
2019-04-08 18:19:44 +0100

    Update feelinganalysis.php

--- feelinganalysis.php
+++ feelinganalysis.php
@@ -1,5 +1,5 @@
 <?php
-  $mysqli=mysqli_connect("ephesus.cs.cf.ac.uk", "c1327916", "dharanboi1995", "c1327916");
+  $mysqli=mysqli_connect("url", "username", "password", "username");
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
